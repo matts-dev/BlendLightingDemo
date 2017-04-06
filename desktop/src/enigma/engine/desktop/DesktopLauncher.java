3792c56c2459f1ad1f5d7cc14ea9c83b4ddf74a6
dev-user <no.email>
2017-04-06 14:47:39 -0400

    init repo

---
+++ desktop/src/enigma/engine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package enigma.engine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import enigma.engine.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
